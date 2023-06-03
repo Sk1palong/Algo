3edb754ca026dd67392c11b335bed36a9ea1dd2b
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-06-03 21:30:45 +0900

    [Bronze V] Title: A×B, Time: 112 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a*b);
+
+
+
+    }
+}
