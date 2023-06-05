a5f7f339288f78daa19a1d6fb3257454cf7980c6
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-06-06 00:32:37 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 112 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 4; i <=N ; i = i+4) {
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
