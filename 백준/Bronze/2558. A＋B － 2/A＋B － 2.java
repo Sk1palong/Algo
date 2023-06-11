d249799c804a7ed1aeab0d477e10653e9580b747
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-06-11 14:51:18 +0900

    [Bronze V] Title: A+B - 2, Time: 108 ms, Memory: 12796 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.nextLine());
+        int b = Integer.parseInt(sc.nextLine());
+
+        System.out.println(a+b);
+    }
+}
