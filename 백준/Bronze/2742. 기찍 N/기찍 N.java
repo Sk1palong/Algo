66915ddfa0bbf0f5aa0bdfb1b7e616c2abb10715
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-06-11 15:51:22 +0900

    [Bronze IV] Title: 기찍 N, Time: 1408 ms, Memory: 33496 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = a; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
