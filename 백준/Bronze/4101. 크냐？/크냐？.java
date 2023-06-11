beaac64759352c0fc47cd80827acc571337eb8f2
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-06-11 16:26:36 +0900

    [Bronze V] Title: 크냐?, Time: 140 ms, Memory: 13136 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0)
+                break;
+            if (a > b)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+        }
+    }
+}
