ff583a81f69ee1a53b11f745dfe0ac7fb90535ac
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-06-12 21:59:41 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 124 ms, Memory: 13084 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = i;
+        }
+        for (int i = 0; i < M; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            while (x < y) {
+                int tmp = arr[y];
+                arr[y] = arr[x];
+                arr[x] = tmp;
+                x++;
+                y--;
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
