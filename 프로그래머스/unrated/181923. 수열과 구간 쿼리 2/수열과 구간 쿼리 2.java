adefd14c08484e5fb25b0eb830fff762ccd4715e
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-06-25 15:27:56 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 2.69 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -3,19 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         int[] answer = new int[queries.length];
-        Arrays.fill(answer,Integer.MAX_VALUE);
-        for (int a = 0; a<queries.length; a++){
-            for(int i = queries[a][0]; i<= queries[a][1]; i++){
-                if(arr[i]>queries[a][2]){
-                    if(answer[a]>arr[i]){
-                        answer[a] = arr[i];
+        Arrays.fill(answer,-1);
+        for(int i = 0; i < queries.length; i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            for(int j = s; j<=e; j++ ){
+                if(arr[j]>k){
+                    answer[i] = (answer[i] == -1) ? arr[j] : Math.min(answer[i],arr[j]) ;
                     }
+                
                 }
             }
-             if(answer[a] == Integer.MAX_VALUE){
-            answer[a] = -1;
-        }
-        }
+        
         return answer;
     }
 }
