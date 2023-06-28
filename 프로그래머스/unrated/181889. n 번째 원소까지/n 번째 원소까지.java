91a72efbd32200bed7ed1977c292aa476f98b017
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-06-28 23:46:57 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        num_list = Arrays.copyOfRange(num_list,0,n);
+        
+        return num_list;
+    }
+}
