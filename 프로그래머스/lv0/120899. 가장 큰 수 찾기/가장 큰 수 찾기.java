3af57ea4d96c02e4fd4982beffadb89f6872004c
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-07-09 14:24:47 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array) {
+        int cnt= 0;
+        int max = array[0];
+        for(int i =0; i<array.length; i++){
+            if(array[i]>max){
+                max = array[i];
+                cnt = i;
+            }
+            
+        }
+        int[] answer = {max,cnt};
+        return answer;
+    }
+}
