f19db1a1fddb16e6092882d49677bb4fe8f870f7
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-09-23 22:52:50 +0900

    [unrated] Title: 덧칠하기, Time: 1.44 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 1;
+        int start = section[0];
+        for (int i = 0; i < section.length; i++) {
+            if(start + m > section[i]){
+                continue;
+            }
+            start = section[i];
+            answer++;
+        }
+        return answer;
+    }
+}
