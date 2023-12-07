4ea4be88b8ee9881323d5666fcd117bb208625a9
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-12-07 11:24:58 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 25.32 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.substring(0, i).lastIndexOf(s.charAt(i));
+            if (s.indexOf(s.charAt(i)) == i) {
+                answer[i] = -1;
+            } else {
+                answer[i] = i - idx;
+            }
+        }
+        return answer;
+
+    }
+}
