25874d69aa00e10394772803dc157034350ca195
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2023-12-15 11:53:57 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.17 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,17 +1,38 @@
+// class Solution {
+//     public int solution(int number, int limit, int power) {
+//         int answer = 0;
+//         for(int i = 1; i <= number ; i++) {
+//             int cnt = 1;
+//             for(int j = 1; j <= i/2 ; j++) {
+//                 if( i % j == 0) {
+//                     cnt++;
+//                 }
+//             }
+//             if(cnt > limit) {
+//                 answer += power;
+//             } else {
+//                 answer += cnt;   
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
+
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        for(int i = 1; i <= number ; i++) {
-            int cnt = 1;
-            for(int j = 1; j <= i/2 ; j++) {
-                if( i % j == 0) {
-                    cnt++;
-                }
+        int[] count = new int[number + 1];    
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            if(cnt > limit) {
+        }
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
                 answer += power;
             } else {
-                answer += cnt;   
+                answer += count[i];
             }
         }
         return answer;
