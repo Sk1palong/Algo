1d49a22040466001b3508a8d87a09adc8f1c63cd
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-02-13 09:54:50 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.38 ms, Memory: 68.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        Arrays.sort(arr);
+        for(int i=1; i<arr.length; i++) {
+            answer = lcm(answer, arr[i]);
+        }
+        return answer;
+    }
+    
+    public int gcd(int a, int b) {
+        while(b != 0) {
+            int tmp = a % b;
+            a = b ;
+            b = tmp;
+        }
+        return a;
+    }
+    
+    public int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
