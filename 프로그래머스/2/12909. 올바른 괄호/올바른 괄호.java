bf559e1dd193ce29f00abbb54183172e6a4ad1e5
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-02-26 17:38:07 +0900

    [level 2] Title: 올바른 괄호, Time: 19.45 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,18 +3,17 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        Stack<Character> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '('){
-                stack.push(s.charAt(i));
-            } else if (s.charAt(i) == ')') {
-                if(stack.isEmpty()){
-                    return false;
-                } else{
-                    stack.pop();
-                }
+        Queue<Character> q = new LinkedList<>();
+        for(int i=0; i<s.length(); i++) {
+            if(q.isEmpty() || s.charAt(i) == '(') {
+                q.add(s.charAt(i));
+            } else if(s.charAt(i) == ')') {
+                q.poll();
             }
         }
-        return stack.isEmpty();
+        if(!q.isEmpty()) {
+            answer = false;
+        }
+        return answer;
     }
 }
