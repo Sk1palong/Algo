33b46f29174ba9fe3c7c452ae2325a5f033bdcc0
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-02-28 17:07:55 +0900

    [level 0] Title: 공 던지기, Time: 0.07 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,12 +1,14 @@
 class Solution {
     public int solution(int[] numbers, int k) {
         int answer = 0;
-        int temp = 0;
+        int idx = 0;
+        
         for(int i=0; i<k-1; i++) {
-            temp += 2;
+            idx += 2;
         }
-        System.out.println(temp%numbers.length);
-        answer = numbers[temp%numbers.length];
+        
+        answer = numbers[idx % numbers.length];
+        
         return answer;
     }
 }
