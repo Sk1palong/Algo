cc7a8edc77f71973c94a9731ffa4dbfb3ea6b51c
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-03-05 17:21:27 +0900

    [level 1] Title: 모의고사, Time: 2.76 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,44 +1,45 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        List<Integer> list = new ArrayList<>();
-        int[] cnt  = new int[3];
-        int[] one = {1,2,3,4,5};
-        int[] two = {2,1,2,3,2,4,2,5};
-        int[] three = {3,3,1,1,2,2,4,4,5,5};
-        int[] ill = new int[answers.length];
-        int[] yee = new int[answers.length];
-        int[] sam = new int[answers.length];
-        for (int i = 0; i < answers.length; i++) {
-            ill[i] = one[i%5];
-            yee[i] = two[i%8];
-            sam[i] = three[i%10];
-            if(ill[i]==answers[i]){
-                cnt[0]++;
-            }if(yee[i]==answers[i]){
-                cnt[1]++;
-            }if(sam[i]==answers[i]){
-                cnt[2]++;
+        List<Integer> li1 = Arrays.asList(1,2,3,4,5);
+        List<Integer> li2 = Arrays.asList(2,1,2,3,2,4,2,5);
+        List<Integer> li3 = Arrays.asList(3,3,1,1,2,2,4,4,5,5);
+        int[] score = new int[3];
+        List<Integer> rank = new ArrayList<>();
+        // int score1 = 0;
+        // int score2 = 0;
+        // int score3 = 0;
+        // Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == li1.get(i%li1.size())) {
+                score[0]++;
+                // map.put(1, score1);
+            }
+            if(answers[i] == li2.get(i%li2.size())) {
+                score[1]++;
+                // map.put(2, score2);
+            }
+            if(answers[i] == li3.get(i%li3.size())) {
+                score[2]++;
+                // map.put(3, score3);
+            }
+        } 
+        
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        for(int i=0; i<score.length; i++) {
+            if(score[i] == max) {
+                rank.add(i+1);
             }
         }
-        int max = cnt[0];
-        for (int i = 1; i < cnt.length; i++) {
-            if(cnt[i]>=max){
-                max = cnt[i];
-            }
+        
+        int[] answer = new int[rank.size()];
+        
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = rank.get(i);
         }
-        for (int i = 0; i < cnt.length; i++) {
-            if(cnt[i] == max){
-                list.add(i+1);
-            }
-        }
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        
         return answer;
     }
 }
