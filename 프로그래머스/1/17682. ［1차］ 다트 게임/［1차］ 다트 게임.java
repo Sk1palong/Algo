5a5518712df26162b32687a9aa6a65710209b9e1
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-03-11 16:19:33 +0900

    [level 1] Title: [1차] 다트 게임, Time: 11.86 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,53 +1,50 @@
-import java.util.*;
-
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
         String numStr = "";
+        int[] dart = new int[3];
         int idx = 0;
-        int[] dart = new int[3]; 
+        
         
         for(int i=0; i<dartResult.length(); i++) {
             char c = dartResult.charAt(i);
             
             if('0' <= c && c <= '9') {
                 numStr += c;
-            } 
-            else if('D' <= c && c <= 'T') {
+            }
+            
+            else if(c == 'S' || c =='D' || c =='T') {
                 int n = Integer.parseInt(numStr);
                 if(c == 'S') {
                     dart[idx] = n;
-                }
-                else if(c == 'D') {
+                } else if(c == 'D') {
                     dart[idx] = (int)Math.pow(n, 2);
-                }
-                else if(c == 'T') {
+                } else if(c == 'T') {
                     dart[idx] = (int)Math.pow(n, 3);
                 }
-                numStr = "";
                 idx++;
-                
+                numStr = "";
             }
+            
             else if(c == '*' || c == '#') {
                 if(c == '*') {
                     if(idx == 1) {
-                        dart[idx-1] *= 2;
-                    }
-                    else {
+                        dart[0] *= 2;
+                    } else {
                         dart[idx-2] *= 2;
                         dart[idx-1] *= 2;
                     }
                 }
-                else if(c == '#') {
+                else {
                     dart[idx-1] *= -1;
                 }
-            }    
+            }
         }
-        System.out.println("dart[0] = " + dart[0]);
-        System.out.println("dart[1] = " + dart[1]);
-        System.out.println("dart[2] = " + dart[2]);
-        answer = dart[0] + dart[1] + dart[2];
-
+        
+        for(int n : dart) {
+            answer += n;
+        }
+        
         return answer;
     }
 }
