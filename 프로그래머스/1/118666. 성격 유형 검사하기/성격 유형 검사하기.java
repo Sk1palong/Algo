5698e5fda7b3154fb975bf231ba6bd581c5f49ec
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-03-13 16:14:54 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 12.38 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -6,15 +6,17 @@ class Solution {
         char[][] cArr = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
         Map<Character, Integer> map = new HashMap<>();
         
-        for(int i=0; i<choices.length; i++) {
+        for(int i=0; i<survey.length; i++) {
+            char ch0 = survey[i].charAt(0);
+            char ch1 = survey[i].charAt(1);
             if(choices[i] < 4) {
-                map.put(survey[i].charAt(0), map.getOrDefault(survey[i].charAt(0), 0) + Math.abs(4-choices[i]));
+                map.put(ch0, map.getOrDefault(ch0, 0) + Math.abs(4-choices[i]));
             }
             else {
-                map.put(survey[i].charAt(1), map.getOrDefault(survey[i].charAt(1), 0) + Math.abs(4-choices[i]));          
+                map.put(ch1, map.getOrDefault(ch1, 0) + Math.abs(4-choices[i]));
             }
         }
-        
+
         for(int i=0; i<cArr.length; i++) {
             if(map.getOrDefault(cArr[i][0], 0) >= map.getOrDefault(cArr[i][1], 0)) {
                 answer += cArr[i][0];
