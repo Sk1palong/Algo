ac01d444d04a1c0ffc513cc04b041097bd09e99d
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-03-21 11:41:16 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,13 +3,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
-        for(int i=0; i<commands.length; i++){
-            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(temp);
-            answer[i] = temp[commands[i][2]-1];
+        int idx = 0;
+        int start;
+        int end;
+        int find;
+        
+        for(int[] command : commands) {
+            start = command[0] - 1;
+            end = command[1];
+            find = command[2] -1;
+            
+            int[] arr = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(arr);
+            
+            answer[idx] = arr[find];
+            idx++;
         }
-
+        
         return answer;
     }
 }
