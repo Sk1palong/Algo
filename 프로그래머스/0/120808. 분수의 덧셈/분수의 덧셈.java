17935f206702a84187ce4cd14cd0aa8475f12d1f
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-04-06 14:46:51 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,18 +1,27 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
-        int a = numer1*denom2;
-        int b = numer2*denom1;
-        int c = denom1*denom2;
-        int d = a+b;
-        for(int i = d-1; i>1; i--){
-            if(d%i==0 && c%i==0){
-                d/=i;
-                c/=i;
-            }
-        }
-        answer[0] = d;
-        answer[1] = c;
+        // 기약분수 == 분자 분모를 최대공약수로 나눈 것
+        // 분수끼리 덧셈 -> 분모끼리 최소공배수가 되도록 분자 분모에 곱한 뒤 더함
+        int denom = lcm(denom1, denom2);
+        int numer = numer1 * (denom/denom1) + numer2 * (denom/denom2);
+        int gcdDN = gcd(Math.max(denom, numer), Math.min(denom, numer));
+        
+        answer[0] = numer/gcdDN;
+        answer[1] = denom/gcdDN;
+        
         return answer;
+    }
+    
+    public int lcm(int a, int b) {
+        int gcdAB = gcd(a, b);
+        return a * b / gcdAB;
+    }
+    
+    public int gcd(int a, int b) {
+        if(b == 0) {
+            return a;
+        }
+        return gcd(b, a%b);
     }
 }
