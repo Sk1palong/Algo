8419373f02b43b569dec284587801e7dcae010b2
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-04-08 14:25:30 +0900

    [level 2] Title: 전화번호 목록, Time: 204.73 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,20 +3,27 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
-//         Map<String, Integer> map = new HashMap<>();
+        // Arrays.sort(phone_book);
+        Set<String> set = new HashSet<>();
         
-//         for(int i=0; i<phone_book.length; i++) {
-//             if(map.get)            
-//             map.put(phone_book[i], map.getOrDefault(phone_book[i], 0) + 1);
-
-//         }
-//         System.out.println(map);
-        for(int i=0; i<phone_book.length-1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
+        for(int i=0; i<phone_book.length; i++) {
+            set.add(phone_book[i]);
+        }
+        
+        for(int i=0; i<phone_book.length; i++) {
+            for(int j=0; j<phone_book[i].length(); j++) {
+                if(set.contains(phone_book[i].substring(0, j))) {
+                    return false;
+                }
             }
         }
+        
+        // for(int i=0; i<phone_book.length-1; i++) {
+        //     if(phone_book[i+1].startsWith(phone_book[i])) {
+        //         return false;
+        //     }
+        // }
+        
         return answer;
     }
 }
