a7acfbbf174850d4e4d97bf2a3b3f0f202d2bb6b
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-04-17 11:42:01 +0900

    [level 2] Title: 올바른 괄호, Time: 16.64 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909') 
 
 ### 성능 요약
 
-메모리: 53.5 MB, 시간: 19.45 ms
+메모리: 53.8 MB, 시간: 16.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 26일 17:38:05
+2024년 04월 17일 11:41:59
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,18 +2,68 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
+        boolean answer = false;
         Queue<Character> q = new LinkedList<>();
-        for(int i=0; i<s.length(); i++) {
-            if(q.isEmpty() || s.charAt(i) == '(') {
-                q.add(s.charAt(i));
-            } else if(s.charAt(i) == ')') {
-                q.poll();
+        char[] cArr = s.toCharArray();
+        
+        for(char c : cArr) {
+            if(q.isEmpty()) {
+                q.add(c);
+            }
+            else {
+                if(c == ')') {
+                    q.poll();
+                }
+                else {
+                    q.add(c);
+                }
             }
         }
-        if(!q.isEmpty()) {
-            answer = false;
+        if(q.isEmpty()) {
+            answer = true;
         }
         return answer;
     }
 }
+// class Solution {
+//     boolean solution(String s) {
+//         boolean answer = true;
+//         Queue<Character> q = new LinkedList<>();
+        
+//         for(int i=0; i<s.length(); i++) {
+//             if(q.isEmpty() || s.charAt(i) == '(') {
+//                 q.add('a');
+//             } else if(s.charAt(i) == ')') {
+//                 q.poll();
+//             }
+//         }
+        
+//         if(!q.isEmpty()) {
+//             answer = false;
+//         }
+        
+//         return answer;
+//     }
+// }
+
+// import java.util.*;
+
+// class Solution {
+//     boolean solution(String s) {
+//         boolean answer = true;
+//         Stack<Character> stack = new Stack<>();
+        
+//         for(int i=0; i<s.length(); i++) {
+//             if(s.charAt(i) == '(') {
+//                 stack.push(s.charAt(i));
+//             } else if (s.charAt(i) == ')') {
+//                 if(stack.isEmpty()) {
+//                     return false;
+//                 } else {
+//                     stack.pop();
+//                 }
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
