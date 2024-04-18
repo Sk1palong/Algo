200aec4695ed27c0c817899ef67e24499cc0e455
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-04-18 20:59:56 +0900

    [level 2] Title: 짝지어 제거하기, Time: 55.00 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 58.9 MB, 시간: 48.12 ms
+메모리: 60.6 MB, 시간: 55.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 15:49:49
+2024년 04월 18일 20:59:53
 
 ### 문제 설명
 
--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,25 +1,26 @@
 import java.util.*;
 
-class Solution{
-    
-    public int solution(String s){
+class Solution {
+    public int solution(String s) {
         int answer = 0;
-        Stack<Character> stack = new Stack<>();
-        // for(s의길이){
-        //     stack에 s.charAt()추가 s.charAt()이 s.peek()과 같다면 추가하지 않고 pop
-        // }
-        for(int i=0; i<s.length(); i++){
-            if(stack.isEmpty()){
-                stack.push(s.charAt(i));
-            } else if(stack.peek() == s.charAt(i)){
-                stack.pop();
-            } else{
-                stack.push(s.charAt(i));
+        Stack<Character> stk = new Stack<>();
+        
+        for(char c : s.toCharArray()) {
+            if(stk.isEmpty()) {
+                stk.push(c);
             }
+            else if(stk.peek() == c) {
+                stk.pop();
+            }
+            else {
+                stk.push(c);
+            }
+            
         }
-        if(stack.isEmpty()){
+        if(stk.isEmpty()) {
             answer = 1;
         }
+        
         return answer;
     }
 }
