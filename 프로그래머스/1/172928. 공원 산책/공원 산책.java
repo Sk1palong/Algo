da6b552dd5171d40ad78cb5ad61de0767a9e7b8c
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-04-30 17:27:14 +0900

    [level 1] Title: 공원 산책, Time: 8.67 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -2,56 +2,61 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        int[] answer = new int[2];
-        int x = 0;
-        int y = 0;       
+        int h = park.length;
+        int w = park[0].length();
+        int row = 0;
+        int col = 0;
         
-        for(int i=0; i<park.length; i++) {
-            for(int j=0; j<park[i].length(); j++) {
+        for(int i=0; i<h; i++) {
+            for(int j=0; j<w; j++) {
                 if(park[i].charAt(j) == 'S') {
-                    x = j;
-                    y = i;
+                    row = i;
+                    col = j;
+                    break;
                 }
             }
         }
         
         for(int i=0; i<routes.length; i++) {
-            for(int j=0; j<routes[i].charAt(2)-'0'; j++) {
-                char op = routes[i].charAt(0);
-                if(op == 'E') {
-                    x++;
-                    if(x == park[y].length() || park[y].charAt(x) == 'X') {
-                        x -= j+1;
+            char dir = routes[i].charAt(0);
+            int times = Integer.parseInt(routes[i].charAt(2)+"");
+            for(int j=0; j<times; j++) {
+                if(dir == 'E') {
+                    col++;
+                    if(!bc(park, row, col, h, w)) {
+                        col -= j+1;
                         break;
                     }
                 }
-                if(op == 'W') {
-                    x--;
-                    if(x < 0 || park[y].charAt(x) == 'X') {
-                        x += j+1;
+                else if(dir == 'W') {
+                    col--;
+                    if(!bc(park, row, col, h, w)) {
+                        col += j+1;
                         break;
                     }
                 }
-                if(op == 'N') {
-                    y--;
-                    if(y < 0 || park[y].charAt(x) == 'X') {
-                        y += j+1;
+                else if( dir == 'N') {
+                    row--;
+                    if(!bc(park, row, col, h, w)) {
+                        row += j+1;
                         break;
                     }
                 }
-                if(op == 'S') {
-                    y++;
-                    if(y == park.length || park[y].charAt(x) == 'X') {
-                        y -= j+1;
+                else {
+                    row++;
+                    if(!bc(park, row, col, h, w)) {
+                        row -= j+1;
                         break;
                     }
-                }
+                }                   
             }
         }
+        int[] answer = {row, col};
         
-        answer[0] = y;
-        answer[1] = x;
-
         return answer;
+    }
+    
+    public boolean bc(String[] park, int row, int col, int h, int w) {
+        return 0 <= row && row < h && 0 <= col && col < w && park[row].charAt(col) != 'X'; 
     }
 }
