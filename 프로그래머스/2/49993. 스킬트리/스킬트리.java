cf0f93ffce9836e0a6119b3a576edc90ac8c3b89
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-05-01 17:53:01 +0900

    [level 2] Title: 스킬트리, Time: 0.29 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        // Queue<Character> q = new LinkedList<>();
-        // for(int i=0; i<skill.length(); i++) {
-        //     q.add(skill.charAt(i));
-        // }
-       loop : for(int i=0; i<skill_trees.length; i++) {
+
+ loop : for(int i=0; i<skill_trees.length; i++) {
             Queue<Character> q = new LinkedList<>();
+     
             for(int j=0; j<skill.length(); j++) {
                 q.add(skill.charAt(j));
             }
+     
             for(int j=0; j<skill_trees[i].length(); j++) {
                 char c = skill_trees[i].charAt(j);
                 if(q.contains(c)) {
@@ -20,12 +19,15 @@ class Solution {
                     }
                     else {
                         q.poll();
+                        if(q.isEmpty()) {
+                            break;
+                        }
                     }
                 }
             }
            answer++;
         }
-        // System.out.println(q);
+        
         return answer;
     }
 }
