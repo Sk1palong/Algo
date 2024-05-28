fa400f7e33e508dfbf7ec46567f1babb249403ac
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-05-28 16:39:45 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 112 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int up = sc.nextInt();
+        int down = sc.nextInt();
+        int height = sc.nextInt();
+
+        int day = (height - down) / (up - down);
+
+        if((height - down) % (up - down) != 0) {
+            day++;
+        }
+
+        System.out.println(day);
+
+    }
+}
