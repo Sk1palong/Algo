e85fbbeb08c3684747185039d9a4a37ea4ce0b9b
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-06-06 01:52:57 +0900

    [Bronze II] Title: 커트라인, Time: 84 ms, Memory: 11852 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int cut = Integer.parseInt(st.nextToken());
+        int[] scores = new int[n];
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
+            int score = Integer.parseInt(st.nextToken());
+            scores[i] = score;
+        }
+
+        Arrays.sort(scores);
+        System.out.println(scores[scores.length - cut]);
+    }
+}
