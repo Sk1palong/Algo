9ed014abcacc12ed414689ef40e469c22027484c
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-06-07 16:57:49 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 556 ms, Memory: 54728 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        int[][] coordinate = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            coordinate[i][0] = Integer.parseInt(st.nextToken());
+            coordinate[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(coordinate, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                int result = o1[0] - o2[0];
+                
+                if (result == 0) {
+                    result = o1[1] - o2[1];
+                }
+                
+                return result;
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            sb.append(coordinate[i][0]).append(" ").append(coordinate[i][1]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
