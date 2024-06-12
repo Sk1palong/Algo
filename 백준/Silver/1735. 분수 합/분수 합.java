c9b70e01e58fac8174f63e816f50d7398a79a30e
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-06-12 18:15:08 +0900

    [Silver III] Title: 분수 합, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int down = lcm(b, d);
+
+        int multi1 = lcm(b, d) / b;
+        int multi2 = lcm(b, d) / d;
+
+        a *= multi1;
+        c *= multi2;
+
+        int up = a + c;
+        int divide = gcd(up, down);
+
+        up /= divide;
+        down /= divide;
+
+        sb.append(up).append(' ').append(down);
+
+        System.out.println(sb);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
