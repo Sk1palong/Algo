fc73900d0bd925fd1c7322f95dc14b02cfed1208
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-06-13 23:05:38 +0900

    [Silver IV] Title: 다음 소수, Time: 360 ms, Memory: 11956 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            long num = Long.parseLong(br.readLine());
+            while (!isPrime(num)) {
+                num++;
+            }
+            sb.append(num).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    public static boolean isPrime(long n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
