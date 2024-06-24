4f318d3d2e2f4b39a36018fc9ceaea84ee895451
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-06-25 00:34:16 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 644 ms, Memory: 98296 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int cnt = 0;
+    static int k;
+    static int ans = -1;
+    static int[] tmp;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        tmp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        merge_sort(arr, 0, n - 1);
+        bw.write(ans+"");
+        bw.flush();
+        bw.close();
+    }
+
+    static void merge_sort(int[] arr, int p, int r) {
+        if (cnt > k) {
+            return;
+        }
+        if (p < r) {
+            int q = (p + r) / 2;
+            merge_sort(arr, p, q);
+            merge_sort(arr, q + 1, r);
+            merge(arr, p, q, r);
+        }
+    }
+
+    static void merge(int[] arr, int p, int q, int r) {
+        int i = p;
+        int j = q + 1;
+        int t = 0;
+
+        while (i <= q && j <= r) {
+            if (arr[i] <= arr[j]) {
+                tmp[t++] = arr[i++];
+            } else {
+                tmp[t++] = arr[j++];
+            }
+        }
+        while (i <= q) {
+            tmp[t++] = arr[i++];
+        }
+        while (j <= r) {
+            tmp[t++] = arr[j++];
+        }
+        i = p;
+        t = 0;
+
+        while (i <= r) {
+            cnt++;
+            if (cnt == k) {
+                ans = tmp[t];
+                break;
+            }
+            arr[i++] = tmp[t++];
+        }
+    }
+}
