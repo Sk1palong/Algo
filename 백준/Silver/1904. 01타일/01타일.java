2c827468a33c86cfd416d120f7662f4501cb99c2
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-07-08 16:27:15 +0900

    [Silver III] Title: 01타일, Time: 232 ms, Memory: 58204 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,34 +1,30 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
- 
-	public static int[] dp = new int[1000001];;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		
-		dp[0] = 0;
-		dp[1] = 1;
-		dp[2] = 2;
- 
-		for(int i = 3; i < dp.length; i++) {
-			dp[i] = -1;
-		}
-		
-		System.out.println(Tile(N));
-		
-	}
-	
-	public static int Tile(int N) {
-		
-		if(dp[N] == -1) {
-			dp[N] = (Tile(N - 1) + Tile((N - 2))) % 15746;
-		}
-		return dp[N];
-	}
- 
+
+    static int[] dp = new int[1000001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        System.out.println(tile(n));
+    }
+
+    static int tile(int n) {
+        if (dp[n] == -1) {
+            dp[n] = (tile(n - 1) + tile(n - 2)) % 15746;
+        }
+
+        return dp[n];
+    }
 }
