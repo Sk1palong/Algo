09f3b1b54c68f18a142517550d3d14a5cfa0c188
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-07-13 14:59:41 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 11836 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        int time = 0;
+        int sum = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            time += sum;
+        }
+        
+        System.out.println(time);
+    }
+
+}
