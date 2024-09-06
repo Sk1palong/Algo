6c8af5876b37519f8ddbfa50eeb4b34a920ad442
Sk1palong <133875448+Sk1palong@users.noreply.github.com>
2024-09-07 00:26:10 +0900

    [Bronze IV] Title: 평균 점수, Time: 60 ms, Memory: 11444 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int score = Math.max(40, Integer.parseInt(br.readLine()));
+            sum += score;
+        }
+
+        System.out.println(sum / 5);
+    }
+}
